8c1baab2ec1db9a4f62b7c50c6ebd9a880e67d98
Tekser15 <dgrib54@gmail.com>
2023-01-22 15:08:43 +0300

    tallwind

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import {Component} from "react";
 
-class App extends Component {
+export default class App extends Component {
 
     render() {
         return (
             <div className="App">
-                <p>Привет 12345</p>
+                <h1 className="text-3xl font-bold underline">
+                    Hello world!
+                </h1>
             </div>
         );
     }
 }
 
-export default App;
+
