11fc189f23ea6466e9b2751f08ebffc7109f18d3
Tekser15 <dgrib54@gmail.com>
2023-01-22 16:21:49 +0300

    header

--- src/modules/problems/Problems.js
+++ src/modules/problems/Problems.js
@@ -1,13 +1,13 @@
 import {Component} from "react";
+import Header from "./Header"
 
 export default class Problems extends Component {
     render() {
         return (
-            <div className="App">
-                <h1 className="text-3xl font-bold underline">
-                    Задачи
-                </h1>
-            </div>
+            <>
+                <Header/>
+
+            </>
         );
     }
 }
