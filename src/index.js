0abe68f4551d9f8b25fee51511490323dd178d26
Tekser15 <dgrib54@gmail.com>
2023-01-22 15:48:36 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Main from './App';
-import Error404 from "./404";
-import Problems from "./Problems"
+import Main from './main/App';
+import Error404 from "./errors/404";
+import Problems from "./problems/Problems"
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
@@ -19,7 +19,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
