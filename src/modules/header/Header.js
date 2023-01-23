fba0adcd7ae3f01e5a3357e7d27e71a8f397e4ec
Tekser15 <dgrib54@gmail.com>
2023-01-24 00:51:39 +0300

    header update

--- src/modules/header/Header.js
+++ src/modules/header/Header.js
@@ -1,32 +1,44 @@
 import {Component} from "react";
 import {Link} from "react-router-dom";
 import '../../index.css'
+import logo from './images/logo.svg'
+
 
 export default class Header extends Component{
     render(){
         return(
             <>
-                <div className="h-8 pt-1">
-                    <Link to="/" >
-                        <span className="pl-72 h-6">
-                            NIKISTRAIN
-                        </span>
+                <div className="h-8 pt-0 pl-72">
+                    <Link to="/">
+                        <div className="inline">
+                        <img src={logo} alt="fireSpot" className="h-6 inline align-middle"/>
+                        <div className="inline text-lg">
+                            <span className="font-medium pl-1 h-6 align-middle" style={{"color": "#AD922A"}}>
+                                NIKIS
+                            </span>
+                            <span className="font-medium h-6 align-middle" style={{"color": "#6D77FA"}}>
+                                TRAIN
+                            </span>
+                        </div>
+                        </div>
                     </Link>
-                    <Link to="/problems">
-                        <span className="font-medium space-x-2.5 pl-2.5 h6">
-                            Задачи
-                        </span>
-                    </Link>
-                    <Link to="/problems">
-                        <span className="font-medium space-x-2.5 pl-2.5 h6">
-                            Обучение
-                        </span>
-                    </Link>
-                    <Link to="/problems">
-                        <span className="font-medium space-x-2.5 pl-2.5 h6">
-                            Контесты
-                        </span>
-                    </Link>
+                    <div className="pl-2 inline text-lg space-x-1 font-medium align-middle">
+                        <Link to="/problems">
+                            <span className="px-3 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+                                Задачи
+                            </span>
+                        </Link>
+                        <Link to="/problems">
+                            <span className="px-3 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+                                Обучение
+                            </span>
+                        </Link>
+                        <Link to="/problems">
+                            <span className="px-3 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+                                Контесты
+                            </span>
+                        </Link>
+                    </div>
                 </div>
                 <hr></hr>
             </>
